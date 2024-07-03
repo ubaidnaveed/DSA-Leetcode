54412b893ab3bd2375e90ebc6e557cfbf12605bb
Ubaid <163641366+ubaidnaveed@users.noreply.github.com>
2024-07-03 08:48:05 +0500

    Time: 1 ms (99.84%), Space: 52.7 MB (64.50%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,18 +1,36 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int majority=0;
         int n = nums.length;
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < n; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-       
-         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > (n/2)) {
-
-                majority = entry.getKey();
+        int vote = 1;
+        int majority=nums[0];
+        for(int i=1; i<n;i++){
+            if(vote==0){
+                vote++;
+                majority=nums[i];
+            }
+            else if(nums[i]==majority){
+                vote++;
+                
+            }else{
+                vote--;
             }
         }
+        
         return majority;
+        
+//         int majority=0;
+//         int n = nums.length;
+//         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+//         for (int i = 0; i < n; i++) {
+//             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+//         }
+       
+//          for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+//             if (entry.getValue() > (n/2)) {
+
+//                 majority = entry.getKey();
+//             }
+//         }
+//         return majority;
     }
 }

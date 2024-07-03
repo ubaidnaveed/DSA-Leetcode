b3391094e3b66a457320afc4aabd9466d440fb54
Ubaid <163641366+ubaidnaveed@users.noreply.github.com>
2024-07-03 09:05:30 +0500

    Time: 19 ms (26.08%), Space: 55.2 MB (83.48%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,22 @@
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         HashSet<Integer> set = new HashSet<>();
+//         for(int i=0; i<nums.length;i++){
+//             if(!set.contains(nums[i])){
+//                 set.add(nums[i]);
+//             }else{
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// }
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int i=0; i<nums.length;i++){
-            if(!set.contains(nums[i])){
-                set.add(nums[i]);
-            }else{
-                return true;
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+               return true;
             }
         }
         return false;

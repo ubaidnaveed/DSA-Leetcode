b3b5c81fcf4482131e5edc3c8aed5438c67237fa
Ubaid <163641366+ubaidnaveed@users.noreply.github.com>
2024-07-31 19:10:32 +0500

    Time: 1 ms (86.37%), Space: 41.7 MB (46.56%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int i=0;
+        StringBuilder merge = new StringBuilder();
+        while(i<word1.length() || i<word2.length()){
+            if(i<word1.length()) {
+                merge.append(word1.charAt(i));
+            }
+            if(i<word2.length()) {
+                merge.append(word2.charAt(i));
+            }
+            i++;
+            
+        }
+        return merge.toString();
+    }
+}

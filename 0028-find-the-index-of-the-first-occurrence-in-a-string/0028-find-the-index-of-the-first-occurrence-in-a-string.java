cbc5e4a4c10cf4f359a651ea504ea2beda8445ab
Ubaid <163641366+ubaidnaveed@users.noreply.github.com>
2024-08-20 16:22:33 +0500

    Time: 0 ms (100.00%), Space: 41.7 MB (11.83%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        for(int i=0;i<=haystack.length()-needle.length();i++){
+            if(haystack.substring(i,needle.length()+i).equals(needle)) return i;
+        }
+        return -1;
+    }
+}

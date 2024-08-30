b5db20e508d27b3c685124f698b5ed4266bd3011
Ubaid <163641366+ubaidnaveed@users.noreply.github.com>
2024-08-30 12:40:34 +0500

    Time: 7 ms (66.00%), Space: 47.6 MB (82.30%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(String str : strs){
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String s = new String(arr);
+            if(!map.containsKey(s)) map.put(s,new ArrayList<>());
+            map.get(s).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
